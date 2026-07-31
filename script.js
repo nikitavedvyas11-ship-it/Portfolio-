7296b9e1a780fb08589fa923852547dd30cf24a6
Kamal Sharma <kamalsharma@Kamals-MacBook-Air.local>
2026-07-31 09:10:07 +0530

    Day 10: Added contact form validation and enhanced contact section

--- script.js
+++ script.js
@@ -38,3 +38,18 @@ topBtn.addEventListener("click",function(){
         behavior:"smooth"
     });
 });
+
+//contact-form validation
+const form=document.querySelector(".contact-form")
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    const name=form.querySelector('input[type="text"]').value.trim();
+    const email=form.querySelector('input[type="email"]').value.trim();
+    const message=form.querySelector("textarea").value.trim();
+    if(name===""||email===""||message===""){
+        alert("Please fill out all fields.")
+        return;
+    }
+    alert("Thank you! Your message has been submitted successfully.");
+    form.reset();
+});
